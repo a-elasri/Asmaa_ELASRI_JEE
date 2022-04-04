0a7a5aae19f49dbeaddcf3ad5ca6e8aab18d62c7
elasri <elasriasmaa5720@gmail.com>
2022-04-04 03:07:25 +0200

    activite 2

---
+++ Activite 2/hospital/src/main/java/ma/enset/hospital/repository/RendezVousRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.hospital.repository;
+
+
+import ma.enset.hospital.entities.RendezVous;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RendezVousRepository extends JpaRepository<RendezVous,Long> {
+}
---
+++ Activite 2/jpa-ManyToMany/src/test/java/ma/enset/jpamanytomany/JpaManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.jpamanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
